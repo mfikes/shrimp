971ec1ab0e5cb33b0ef1300f37f8cdb73771e092
Mike Fikes <mike@fikesfarm.com>
2014-10-20 15:57:08 -0400

    Add externs to support optimized builds

---
+++ ClojureScript/externs.js
@@ -0,0 +1,7 @@
+var DatabaseManager = {};
+DatabaseManager.createShrimpFetchedResultsController = function() {};
+
+var Shrimp = {};
+Shrimp.name = function() {};
+Shrimp.desc = function() {};
+Shrimp.edibleFlag = function() {};
