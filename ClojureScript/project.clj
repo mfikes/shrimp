7b9ef724b23d1f5d97ae5251bc391f256df8a3d0
Mike Fikes <mike@fikesfarm.com>
2015-01-10 21:28:23 -0500

    Depend on Goby ClojureScript JAR from Clojars

--- ClojureScript/project.clj
+++ ClojureScript/project.clj
@@ -3,28 +3,26 @@
             :dependencies [[org.clojure/clojure "1.6.0"]
                            [org.clojure/clojurescript "0.0-2511"]
                            [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                           [goby "0.1.0"]
                            [weasel "0.4.2"]]
             :plugins [[lein-cljsbuild "1.0.3"]
                       [jarohen/simple-brepl "0.1.2"]]
-            :source-paths ["goby-src"
-                           "src"]
+            :source-paths ["src"]
             :brepl {:ip "127.0.0.1"}
             :cljsbuild {:builds {:dev
-                                  {:source-paths ["goby-src"
-                                                  "src"
+                                  {:source-paths ["src"
                                                   "dev-src"]
                                    :compiler     {:output-to     "js/main.js"
                                                   :optimizations :whitespace
                                                   :static-fns    false
-                                                  :externs       ["goby-externs.js" "externs.js"]
+                                                  :externs       ["externs.js"]
                                                   :pretty-print  true}}
                                  :rel
-                                  {:source-paths ["goby-src"
-                                                  "src"
+                                  {:source-paths ["src"
                                                   "rel-src"]
                                    :compiler     {:output-to     "js/main.js"
                                                   :optimizations :advanced
                                                   :static-fns    true
-                                                  :externs       ["goby-externs.js" "externs.js"]
+                                                  :externs       ["externs.js"]
                                                   :pretty-print  false
                                                   :pseudo-names  false}}}})
