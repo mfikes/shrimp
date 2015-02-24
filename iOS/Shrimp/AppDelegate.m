61b0344900e56755fc2f0cc54b6801c6fc144513
Mike Fikes <mike@fikesfarm.com>
2015-02-24 11:39:31 -0500

    Delegate to Ambly for bootstrapping impl

--- iOS/Shrimp/AppDelegate.m
+++ iOS/Shrimp/AppDelegate.m
@@ -21,59 +21,7 @@ void uncaughtExceptionHandler(NSException *exception) {
     NSLog(@"Stack Trace: %@", [exception callStackSymbols]);
 }
 
-
 @implementation AppDelegate
-
--(void)bootstrapContext:(JSContext*)context
-{
-    // Setup CLOSURE_IMPORT_SCRIPT
-    [context evaluateScript:@"CLOSURE_IMPORT_SCRIPT = function(src) { require('goog/' + src); return true; }"];
-    
-    // TODO there is a path here assuming "out"
-    
-    // Load goog base
-    NSString *basePath = [[NSBundle mainBundle] pathForResource:@"out/goog/base" ofType:@"js"];
-    NSString *baseScriptString = [NSString stringWithContentsOfFile:basePath encoding:NSUTF8StringEncoding error:nil];
-    [context evaluateScript:baseScriptString];
-    
-    // Load the deps file
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"main" ofType:@"js"];
-    NSString *scriptString = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
-    NSAssert(scriptString != nil, @"The JavaScript text could not be loaded");
-    [context evaluateScript:scriptString];
-    
-    [context evaluateScript:@"goog.isProvided_ = function(x) { return false; };"];
-    
-    [context evaluateScript:@"goog.require = function (name) { return CLOSURE_IMPORT_SCRIPT(goog.dependencies_.nameToPath[name]); };"];
-    
-    [context evaluateScript:@"goog.require('cljs.core');"];
-    
-    // redef goog.require to track loaded libs
-    [context evaluateScript:@"cljs.core._STAR_loaded_libs_STAR_ = new cljs.core.PersistentHashSet(null, new cljs.core.PersistentArrayMap(null, 1, ['cljs.core',null], null), null);\n"
-     "\n"
-     "goog.require = (function (name,reload){\n"
-     "   if(cljs.core.truth_((function (){var or__4112__auto__ = !(cljs.core.contains_QMARK_.call(null,cljs.core._STAR_loaded_libs_STAR_,name));\n"
-     "       if(or__4112__auto__){\n"
-     "           return or__4112__auto__;\n"
-     "       } else {\n"
-     "           return reload;\n"
-     "       }\n"
-     "   })())){\n"
-     "       cljs.core._STAR_loaded_libs_STAR_ = cljs.core.conj.call(null,(function (){var or__4112__auto__ = cljs.core._STAR_loaded_libs_STAR_;\n"
-     "           if(cljs.core.truth_(or__4112__auto__)){\n"
-     "               return or__4112__auto__;\n"
-     "           } else {\n"
-     "               return cljs.core.PersistentHashSet.EMPTY;\n"
-     "           }\n"
-     "       })(),name);\n"
-     "       \n"
-     "       return CLOSURE_IMPORT_SCRIPT((goog.dependencies_.nameToPath[name]));\n"
-     "   } else {\n"
-     "       return null;\n"
-     "   }\n"
-     "});"];
-
-}
 
 -(void)requireAppNamespaces:(JSContext*)context
 {
@@ -140,7 +88,9 @@ void uncaughtExceptionHandler(NSException *exception) {
     
     NSLog(@"Initializing ClojureScript");
     
-    [self bootstrapContext:self.contextManager.context];
+    [self.contextManager bootstrapWithDepsFilePath:[[NSBundle mainBundle] pathForResource:@"main" ofType:@"js"]
+                                      googBasePath:[[NSBundle mainBundle] pathForResource:@"out/goog/base" ofType:@"js"]];
+    
     [self requireAppNamespaces:self.contextManager.context];
     
     self.cljsManager = [[GBYManager alloc] initWithInitFnName:@"init!" inNamespace:@"shrimp.core" withContext:self.contextManager.context];
