0f8f49549a539461ac65a90e26108c512acc1e2f
Mike Fikes <mike@fikesfarm.com>
2015-03-09 14:52:21 -0400

    Update to ClojureScript 0.0-3053
    Also, eliminate compiler options and rely on defaults.

--- ClojureScript/project.clj
+++ ClojureScript/project.clj
@@ -1,7 +1,7 @@
 (defproject shrimp "0.1.0-SNAPSHOT"
   :description "Demo project for Goby."
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3030"]
+                 [org.clojure/clojurescript "0.0-3053"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [goby "0.2.0"]
                  [org.omcljs/ambly "0.1.0-SNAPSHOT"]]
@@ -11,10 +11,7 @@
   :cljsbuild {:builds {:dev
                        {:source-paths ["src"]
                         :compiler     {:output-dir     "out"
-                                       :output-to      "js/main.js"
-                                       :optimizations  :none
-                                       :cache-analysis true
-                                       :source-map     true}}
+                                       :output-to      "js/main.js"}}
                        :rel
                        {:source-paths ["src"]
                         :compiler     {:output-to     "js/main.js"
