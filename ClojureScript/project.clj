01229bac6010b0802a7be97d400d362eac7cf9fe
Mike Fikes <mike@fikesfarm.com>
2015-05-09 08:56:09 -0400

    Update to ClojureScript 0.0-3255

--- ClojureScript/project.clj
+++ ClojureScript/project.clj
@@ -1,7 +1,7 @@
 (defproject shrimp "0.1.0-SNAPSHOT"
   :description "Demo project for Goby."
   :dependencies [[org.clojure/clojure "1.7.0-beta2"]
-                 [org.clojure/clojurescript "0.0-3246"]
+                 [org.clojure/clojurescript "0.0-3255"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [goby "0.3.1"]
                  [org.omcljs/ambly "0.1.0-SNAPSHOT"]]
