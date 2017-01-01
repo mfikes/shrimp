d6afd084df54787fa1b714607268fdfa80b5a3ce
Mike Fikes <mike@fikesfarm.com>
2017-01-01 11:10:41 -0500

    Update deps

--- ClojureScript/project.clj
+++ ClojureScript/project.clj
@@ -1,11 +1,11 @@
 (defproject shrimp "0.1.0-SNAPSHOT"
   :description "Demo project for Goby."
-  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
-                 [org.clojure/clojurescript "0.0-3308"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.293"]
+                 [org.clojure/core.async "0.2.395"]
                  [goby "0.3.1"]
-                 [org.omcljs/ambly "0.7.0"]]
-  :plugins [[lein-cljsbuild "1.0.6"]]
+                 [org.omcljs/ambly "0.8.0-SNAPSHOT"]]
+  :plugins [[lein-cljsbuild "1.1.5"]]
   :source-paths ["src"]
   :clean-targets ["target" "out"]
   :cljsbuild {:builds {:dev
